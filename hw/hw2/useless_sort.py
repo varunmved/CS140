6ed25a609bf8f91662d4a95960abcbd17aee169f
Varun Ved <varunmved@gmail.com>
2016-02-15 15:27:22 -0800

    commited graph and fixed code

--- hw/hw2/useless_sort.py
+++ hw/hw2/useless_sort.py
@@ -1,28 +1,53 @@
-import matplotlib as mp
+#!/usr/bin/env python
 
 #a divide and conquer  with run time T(n) = 2T(n/3) + O(n^2)
 #this implies n/3 runtime in our sort, and n^2 runtime within each partion
 
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
-def useless_sort(a, low, mid, high):
+
+def useless_sort(a):
     #check
     al = len(a)
     if al < 3:
-        return a
-    
+        return 
+
+    #split the arrays into n/3
     first_third = (1/3)*al
     second_third = (2/3)*al
     
     a0 = a[0:first_third]
     a1 = a[first_third+1:second_third]
     a2 = a[second_third+1:]
-
-
-
-    #check funny
-    if (a[0:(1/3)*al)
-
-    #split
-    useless_sort(a[0:(1/3)*al])
-    useless_sorta[
     
+    #do 2 sorts of n/3 length
+    useless_sort(a1)
+    useless_sort(a2)
+
+a = [1,2,3]
+x_axis = [1,2,3]
+#y_axis = [1455578571.091253,1455578571.091253,1455578571.091253]
+y_axis = [0,0,0]
+c = 0
+total = 0
+for i in xrange(4,600):
+    a.append(i)
+    x_axis.append(i)
+    #n^2
+    start = time.time()
+    useless_sort(a)
+    for i in a:
+        for j in a:
+            c+=(i+j)
+    end = time.time()
+    total+=(end-start)
+    y_axis.append(end-start)
+
+plt.plot(x_axis,y_axis)
+plt.show()
+print(x_axis)
+print(y_axis)
+print(c)
+print(total)
