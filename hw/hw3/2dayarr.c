3656e95d225fcfd1fb5caebc32fbda2f02251e15
Varun Ved <varunmved@gmail.com>
2016-02-23 16:08:25 -0800

    commiting my c code for hw3

---
+++ hw/hw3/2dayarr.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+int binarySearch(int[][] arr, int key, int low, int high);
+int arrLength(int )
+
+
+int main()
+{
+    int x = 4;
+    int y = 4;
+    int mat[x][y] = { {10, 20, 30, 40},
+                    {15, 25, 35, 45},
+                    {27, 29, 37, 48},
+                    {32, 33, 39, 50},
+                  };
+    
+    int low = mat[0][0];
+    int high = mat[x][y];
+    int key = 29; 
+    
+    binarySearch(mat,key,low,high);
+}
+
+int binarySearch(int[][] arr, int key, int low, int high)
+{
+    if(low < 4 && high > -1)
+    {
+        if(arr[i][j] == key)
+        {
+            return 1;
+        }
+        else if(arr[i]
+    }
+
+}
---
+++ hw/hw3/2dbinary.py
@@ -0,0 +1,16 @@
+#import matplotlib.plot as pyplot
+import numpy as np
+
+def binarySearch(arr, index):
+    if len(arr) ==0:
+        return False
+    else:
+        midpoint = len(arr)/2
+
+
+#twodarr[10][10]
+a = np.zeros(4,4)
+#mat = [[10, 20, 30, 40],[15, 25, 35, 45],[27, 29, 37, 48], [32, 33, 39, 50]]
+#[[10, 20, 30, 40],[15, 25, 35, 45],[27, 29, 37, 48], [32, 33, 39, 50]]
+
+#print(len(mat))  
